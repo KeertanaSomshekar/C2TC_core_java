dd7a36e40967073f9edba26d573ff88dd4a32dd3
KeertanaSomshekar <keertanas14@gmail.com>
2022-02-21 13:30:49 +0530

    Core Java Day-10

---
+++ 18Feb2022/src/ThreadDemo.java
@@ -0,0 +1,32 @@
+
+class A extends Thread{
+	public void run() {
+		System.out.println("ID of A: "+Thread.currentThread().getId());
+	}
+}
+
+class B extends Thread{
+	public void run() {
+		System.out.println("ID of B: "+Thread.currentThread().getId());
+	}
+}
+
+class C extends Thread{
+	public void run() {
+		System.out.println("ID of C: "+Thread.currentThread().getId());
+	}
+}
+
+public class ThreadDemo {
+	
+	public static void main(String[] args) {
+		A a=new A();
+		a.start();
+		B b=new B();
+		b.start();
+		C c=new C();
+		c.start();
+
+	}
+
+}
