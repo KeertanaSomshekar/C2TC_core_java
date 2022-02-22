1d81810a05cd482d31990dc93e4cbbabc039f66b
KeertanaSomshekar <keertanas14@gmail.com>
2022-02-22 09:05:50 +0530

    Core Java Day-11

---
+++ 21Feb2022/src/StaticDemo2.java
@@ -0,0 +1,17 @@
+
+class Main1 {
+	static void func(int a) throws Exception{
+		System.out.println(10/a);
+	}
+}
+
+public class StaticDemo2 extends Main1 {
+	public static void main(String[] args) {
+		try {
+			func(10);
+			func(0);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
