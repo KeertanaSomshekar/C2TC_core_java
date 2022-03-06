28cc33f99109180e51618a0f9ddf824f41ff0a43
KeertanaSomshekar <keertanas14@gmail.com>
2022-03-06 10:29:43 +0530

    Core Java Day-14

--- 24Feb2022/src/collectionsDataStructures/Program01.java
+++ 24Feb2022/src/collectionsDataStructures/Program01.java
@@ -4,20 +4,21 @@ import java.util.*;
 
 public class Program01 {
 	void Display(List<String> list) {
-		Iterator<String> obj=list.iterator();
-		while(obj.hasNext()) {
-			System.out.println("Element is: "+obj.next());
+		Iterator<String> obj = list.iterator();
+		while (obj.hasNext()) {
+			System.out.println("Element is: " + obj.next());
 		}
 	}
+
 	public static void main(String args[]) {
-		List<String> obj1=new LinkedList<>();
+		List<String> obj1 = new LinkedList<>();
 		obj1.add("abc");
 		obj1.add("def");
 		obj1.add("ghi");
 		obj1.add("jkl");
 		obj1.add("mno");
-		System.out.println("Element is: "+obj1);
-		Program01 p1=new Program01();
+		System.out.println("Element is: " + obj1);
+		Program01 p1 = new Program01();
 		p1.Display(obj1);
 	}
 }
