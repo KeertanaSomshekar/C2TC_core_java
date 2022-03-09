bca2c606515b000f78d61a21485a2117cdfb09ea
KeertanaSomshekar <keertanas14@gmail.com>
2022-03-09 09:22:09 +0530

    Core Java Day-22

---
+++ 08Mar2022/src/Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo implements Runnable{
+
+	@Override
+	public void run() {
+		try {
+			int i = 0;
+			while(i <= 10) {
+				System.out.println(i);
+				i++;
+				Thread.sleep(5000);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ 08Mar2022/src/DemoMain.java
@@ -0,0 +1,13 @@
+
+public class DemoMain {
+
+	public static void main(String[] args) {
+		Demo d1 = new Demo();
+		Demo d2 = new Demo();
+		
+		Thread t1 = new Thread(d1);
+		Thread t2 = new Thread(d2);
+		t1.start();
+		t2.start();
+	}
+}
